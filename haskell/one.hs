c7ece805f9c9cf3f5f942e500b986186553f8354
Paul Sabda <sabdaps@gmail.com>
2014-10-26 19:02:03 +0700

    updates

--- haskell/one.hs
+++ haskell/one.hs
@@ -1,8 +1,26 @@
-module One where
-
 import Data.List
 
 sqr x = x * x
 
 add2 x = x + 2
 
+sum' xs
+  | null xs = 0
+  | otherwise = (head xs) + (sum (tail xs))
+
+sum'' [] = 0
+sum'' (x:xs) = x + (sum'' xs)
+
+faktorial n
+  | n == 0 = 1
+  | otherwise = n * (faktorial (pred n))
+
+fakt 0 = 1
+fakt n = n * (fakt (pred n))
+
+
+sum''' [] = 0
+sum''' (x:xs) = x + sum''' xs
+
+prod' [] = 1
+prod' (x:xs) = x * prod' xs
